f5eb7e95a9c61edf831d7b7675a3c8c2a907d304
Dasun Pubudumal <dasun.15@cse.mrt.ac.lk>
2018-01-17 17:09:29 +0530

    chart service

---
+++ client/src/app/services/chart.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import {User} from "../models/user";
+
+@Injectable()
+export class ChartService {
+
+  private chartData: any[];
+  private chartLabels: any[];
+
+  constructor() {}
+
+}
