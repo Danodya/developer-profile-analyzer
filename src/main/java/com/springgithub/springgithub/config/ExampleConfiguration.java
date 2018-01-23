d9e617cc6fdf61fa7de418ecad081a7e321ac2f0
Dasun Pubudumal <dasun.15@cse.mrt.ac.lk>
2018-01-23 17:13:41 +0530

    config separation

--- src/main/java/com/springgithub/springgithub/config/ExampleConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.springgithub.springgithub.config;
-
-public class ExampleConfiguration {
-    public static final String client_id = "2c77c5a8d6e0519eb3a5";
-    public static final String client_secret = "04ba9edca249e4adf378919a5a1d7e36fad00e96";
-    public static final String token = "";
-}
