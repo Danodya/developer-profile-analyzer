950924b0b2c8de525c895a544191003570aa9808
Dasun Pubudumal <dasun.15@cse.mrt.ac.lk>
2018-04-17 10:52:02 +0530

    deploy

---
+++ src/main/java/com/springgithub/springgithub/config/Configuration.java
@@ -0,0 +1,17 @@
+package com.springgithub.springgithub.config;
+
+public class Configuration {
+    public static final String GITHUB_CLIENT_ID = "a63bb0776b70be956f87";
+    public static final String GITHUB_CLIENT_SECRET = "cdc52a22a2d4f2f8f05f961409bdcd43f29b1550";
+    public static final String GITHUB_TOKEN = "8820b371d630bae345f92ad8ba868bf5733e551c";
+
+    //    Stack Overflow
+    public static final String STACK_KEY = "WaTAupj4SVFVSUbkqe0jlQ((";
+    public static final String SITE = "stackoverflow";
+
+    //    Twitter
+    public static final String TWITTER_CONSUMER_KEY = "RslTXjTdnEwRLcOXHyaK1oxmA";
+    public static final String TWITTER_CONSUMER_SECRET = "LUYwCQVkZ4bwDncTiTnxRU0Y1LYzgpuxlcM5gVrp3rLgfDQBBJ";
+    public static final String TWITTER_ACCESS_TOKEN = "949848603522039808-i4esEowB0UcQy6rgzXyHVhCrEzWegQ7";
+    public static final String TWITTER_ACCESS_TOKEN_SECRET = "GBxIDkIHREOzQEnClIfTbSzTqIV6rnjfP7lJxMGZnjO9c";
+}
